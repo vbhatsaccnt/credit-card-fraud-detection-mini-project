68392b8bd6087e0fd993a1c67bc0197b19094a46
Vikas Bhat <67544433+vbhatsaccnt@users.noreply.github.com>
2023-07-27 17:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-from flask import Flask, render_template, request, flash, redirect
+import streamlit as st
 import pickle
 import numpy as np
-
-app = Flask(__name__)
 
 model = pickle.load(open("final_model.pkl", "rb"))
 
 def predict_default(features):
-
-    features = np.array(features).astype(np.float64).reshape(1,-1)
-    
+    features = np.array(features).astype(np.float64).reshape(1, -1)
     prediction = model.predict(features)
     probability = model.predict_proba(features)
-
     return prediction, probability
 
-@app.route("/", methods = ['GET','POST'])
-def home():
-
+def main():
+    st.title("Credit Default Prediction")
+    
     education_status = ["Graduate School", "University", "High School", "Others"]
-    marital_status = ["Married","Single", "Others"]
-
+    marital_status = ["Married", "Single", "Others"]
+    
     payment_status = [
         "Account started that month with a zero balance, and never used any credit",
         "Account had a balance that was paid in full",
@@ -40,37 +29,47 @@ def home():
         "Payment delay for 7 month",
         "Payment delay for 8 month",   
     ]
-
-    alert_message = False
-    success_message = False
-
-    try:
-        if request.method == 'POST':
-
-            features = request.form.to_dict()
+    
+    st.subheader("Please enter the following information:")
+    
+    features = {}
+    features['EDUCATION'] = st.selectbox("Education", education_status)
+    features['MARRIAGE'] = st.selectbox("Marital Status", marital_status)
+    features['PAY_1'] = st.selectbox("Payment Status", payment_status)
+    
+    features['LIMIT_BAL'] = st.number_input("Credit Limit", min_value=0)
+    features['AGE'] = st.number_input("Age", min_value=0)
+    
+    features['BILL_AMT1'] = st.number_input("Bill Amount 1", min_value=0)
+    features['BILL_AMT2'] = st.number_input("Bill Amount 2", min_value=0)
+    features['BILL_AMT3'] = st.number_input("Bill Amount 3", min_value=0)
+    features['BILL_AMT4'] = st.number_input("Bill Amount 4", min_value=0)
+    features['BILL_AMT5'] = st.number_input("Bill Amount 5", min_value=0)
+    features['BILL_AMT6'] = st.number_input("Bill Amount 6", min_value=0)
+    
+    features['PAY_AMT1'] = st.number_input("Payment Amount 1", min_value=0)
+    features['PAY_AMT2'] = st.number_input("Payment Amount 2", min_value=0)
+    features['PAY_AMT3'] = st.number_input("Payment Amount 3", min_value=0)
+    features['PAY_AMT4'] = st.number_input("Payment Amount 4", min_value=0)
+    features['PAY_AMT5'] = st.number_input("Payment Amount 5", min_value=0)
+    features['PAY_AMT6'] = st.number_input("Payment Amount 6", min_value=0)
+    
+    if st.button("Predict"):
+        try:
             features['EDUCATION'] = education_status.index(features['EDUCATION']) + 1
             features['MARRIAGE'] = marital_status.index(features['MARRIAGE']) + 1
             features['PAY_1'] = payment_status.index(features['PAY_1']) - 2
-
+            
             actual_feature_names = ['LIMIT_BAL', 'EDUCATION', 'MARRIAGE', 'AGE', 'PAY_1', 'BILL_AMT1', 'BILL_AMT2', 'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6', 'PAY_AMT1', 'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6']
             feature_values = [features[i] for i in actual_feature_names]
-
+            
             prediction, probability = predict_default(feature_values)
             if prediction[0] == 1:
-                alert_message = "This account will be defaulted with a probability of {}%.".format(round(np.max(probability)*100, 2))
+                st.error("This account will be defaulted with a probability of {}%.".format(round(np.max(probability)*100, 2)))
             else:
-                success_message = "This account will not be defaulted with a probability of {}%.".format(round(np.max(probability)*100, 2))
-    except:
-        alert_message = "Please enter relevant information."
+                st.success("This account will not be defaulted with a probability of {}%.".format(round(np.max(probability)*100, 2)))
+        except:
+            st.error("Please enter relevant information.")
 
-    return render_template("home.html", education_status = education_status, marital_status = marital_status, payment_status = payment_status, alert_message = alert_message, success_message = success_message)
-
-if __name__ == '__main__':
-    app.run(debug = True)
-
-
-# In[ ]:
-
-
-
-
+if __name__ == "__main__":
+    main()
